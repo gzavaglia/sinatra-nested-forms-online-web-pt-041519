11b102855742dd0e170cda3224ce902125614ef0
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-07-26 01:41:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,9 @@ module FormsLab
     
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
-      params[:pirate][:ships].each do ||
+      params[:pirate][:ships].each do |details|
+        
+      end
     end
     
   end
